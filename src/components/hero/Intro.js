c0eab71f1ccecfbb7de556a619586a2e0bd1f2c9
nwaose victor <nwaosevictor1234@gmail.com>
2021-11-11 13:03:53 +0100

    added cahges in the intro folder

--- src/components/hero/Intro.js
+++ src/components/hero/Intro.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { init } from "ityped";
 
 import "./Intro.scss";
+import Victor from "../../assets/vic.png";
 
 const Intro = () => {
-    return <div className="intro" id="intro"></div>;
+    const textRef = useRef();
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            strings: ["Front-end Developer"],
+        });
+    }, []);
+
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer ">
+                    <img src={Victor} alt="victor" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>hi I'm </h2>
+                    <h1>Nwaose Victor</h1>
+                    <h3>
+                        Freelance {""}
+                        <span ref={textRef}></span>
+                    </h3>
+                </div>
+                <a href="#protfolio">
+                    <RiArrowDropDownLine
+                        className="dropIcon"
+                        style={{ width: "200px", height: "50px" }}
+                    />
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Intro;
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,7 +3,11 @@ import React from "react";
 import "./Portfolio.scss";
 
 const Portfolio = () => {
-    return <div className="portfolio" id="portfolio"></div>;
+    return (
+        <div className="portfolio" id="portfolio">
+            <h1>Portfolios</h1>
+        </div>
+    );
 };
 
 export default Portfolio;
