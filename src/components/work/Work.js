0890a7a84c185a44ae6c6b21bbb62f42bde6212c
nwaose victor <nwaosevictor1234@gmail.com>
2021-11-11 23:54:21 +0100

    added data.js

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { FaMobile, FaGithub, FaChrome } from "react-icons/fa";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+
+import { projects } from "../../data/Data";
 
 import "./Work.scss";
 
 const Work = () => {
-    return <div className="work" id="work"></div>;
+    return (
+        <div className="work" id="work">
+            <div className="slider">
+                {projects?.map((project) => (
+                    <div className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <FaMobile className="img" />
+                                </div>
+                                <h2>{project?.title}</h2>
+                                <p style={{ paddingLeft: "10px" }}>
+                                    {project?.desc}
+                                </p>
+                                <span>{project?.Projects}</span>
+
+                                <div>
+                                    <FaGithub
+                                        className="img"
+                                        onClick={() =>
+                                            window.location.href(
+                                                `${project?.github}`
+                                            )
+                                        }
+                                    />
+                                    <FaChrome className="img" />
+                                </div>
+                            </div>
+                            <div className="right">
+                                <img src={project?.image} alt="pic" />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <AiOutlineArrowLeft className="arrow left" />
+            <AiOutlineArrowRight className="arrow right" />
+        </div>
+    );
 };
 
 export default Work;
---
+++ src/data/Data.js
@@ -0,0 +1,44 @@
+export const projects = [
+    {
+        title: "Crypto APP",
+        desc: "Allow you track and provide your vital information about all crypto coin and the trending coin. Build with React-app Material-UI, Gecko coin api and Firebase version 9",
+        image: "https://wi.wallpapertip.com/wsimgs/15-156939_book-hotel-with-us.jpg",
+        Projects: "Crypto",
+        github: "https://github.com/victornwaose/crypto-app",
+    },
+    {
+        title: "Travel Advisor",
+        desc: "Shows your the best hotels, restaurants and cite seeing around any country of the world  according to rating build with React-app, Material-UI Travel-advisor api and geo-location ",
+        image: "https://wi.wallpapertip.com/wsimgs/15-156939_book-hotel-with-us.jpg",
+        Projects: "Travel-Agent App",
+        github: "https://github.com/victornwaose/crypto-app",
+    },
+    {
+        title: "Apartment- listing",
+        desc: "Provide your with accessibility to sell, lend and buy house with the best of disclose bring you closer to your dream house build with React-app Node.js, Express.js, Firebase v9 and CSS",
+        image: "https://wi.wallpapertip.com/wsimgs/15-156939_book-hotel-with-us.jpg",
+        Projects: "Apartment- listing",
+        github: "https://github.com/victornwaose/crypto-app",
+    },
+    {
+        title: "WatchList",
+        desc: "Show you call Tv movie list and information about your favorite movie Build with React-app IMDB-api",
+        image: "https://wi.wallpapertip.com/wsimgs/15-156939_book-hotel-with-us.jpg",
+        Projects: "WatchList",
+        github: "https://github.com/victornwaose/crypto-app",
+    },
+    {
+        title: "Quiz App",
+        desc: "Test your knowledge on React and JavaScript  build with REact-app CSS and dummy data",
+        image: "https://wi.wallpapertip.com/wsimgs/15-156939_book-hotel-with-us.jpg",
+        Projects: "Quiz App",
+        github: "https://github.com/victornwaose/crypto-app",
+    },
+    {
+        title: "Tip Calculator",
+        desc: "tip calculator complete a tes from front-end Mentor",
+        image: "https://wi.wallpapertip.com/wsimgs/15-156939_book-hotel-with-us.jpg",
+        Projects: "Front-end Mentor Tip calculator",
+        github: "https://github.com/victornwaose/crypto-app",
+    },
+];
