900fa17f174331dd96e0b321494f1ff313d4dfcc
nwaose victor <nwaosevictor1234@gmail.com>
2021-11-12 01:35:28 +0100

    finished work,js

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaMobile, FaGithub, FaChrome } from "react-icons/fa";
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
 
 import { projects } from "../../data/Data";
-
 import "./Work.scss";
 
 const Work = () => {
+    const [currentSlider, setCurrentSlider] = useState(0);
+
+    const handleClick = (direction) => {
+        direction === "left"
+            ? setCurrentSlider(currentSlider > 0 ? currentSlider - 1 : 6)
+            : setCurrentSlider(
+                  currentSlider < projects.length - 1 ? currentSlider + 1 : 0
+              );
+    };
     return (
         <div className="work" id="work">
-            <div className="slider">
+            <div
+                className="slider"
+                style={{ transform: `translateX(-${currentSlider * 100}vw)` }}
+            >
                 {projects?.map((project) => (
                     <div className="container">
+                        <h1>Projects</h1>
                         <div className="item">
                             <div className="left">
                                 <div className="leftContainer">
@@ -23,16 +35,21 @@ const Work = () => {
                                 </p>
                                 <span>{project?.Projects}</span>
 
-                                <div>
+                                <div className="link">
                                     <FaGithub
-                                        className="img"
+                                        style={{ marginRight: "38px" }}
+                                        className="img-git"
                                         onClick={() =>
-                                            window.location.href(
-                                                `${project?.github}`
-                                            )
+                                            window.open(project?.github)
                                         }
                                     />
-                                    <FaChrome className="img" />
+                                    <FaChrome
+                                        className="img-chrome"
+                                        style={{ marginLeft: "38px" }}
+                                        onClick={() =>
+                                            window.open(project?.web)
+                                        }
+                                    />
                                 </div>
                             </div>
                             <div className="right">
@@ -42,8 +59,14 @@ const Work = () => {
                     </div>
                 ))}
             </div>
-            <AiOutlineArrowLeft className="arrow left" />
-            <AiOutlineArrowRight className="arrow right" />
+            <AiOutlineArrowLeft
+                className="arrow left"
+                onClick={() => handleClick("left")}
+            />
+            <AiOutlineArrowRight
+                className="arrow right"
+                onClick={() => handleClick("right")}
+            />
         </div>
     );
 };
